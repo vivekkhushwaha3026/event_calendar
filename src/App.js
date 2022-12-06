a52e5a34a0eab43fec9ed08d516313bab5395a67
vivek khushwaha <vishalpedhadiya.2611@gmail.com>
2022-12-06 15:13:06 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import "@mobiscroll/react/dist/css/mobiscroll.min.css";
+import EventHandler from "./Component/EventCalendar";
+
+function App() {
+  return (
+    <div>
+      <EventHandler />
+    </div>
+  );
+}
+
+export default App;
